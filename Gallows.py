5e6287c46408fe140d79166ebb1f3ee3d012259a
Brskm <94848740+Brskm@users.noreply.github.com>
2022-04-13 18:38:34 +0300

    Виселица, игра на питоне

---
+++ Gallows.py
@@ -0,0 +1,58 @@
+import random
+
+spis_word = ["Горилла", "Человек", "Время", "Жизнь", "Вопрос", "Ребенок", "Конец", "Система", "Начало"]
+sims = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
+        'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+word = random.choice(spis_word)
+finish = True
+used_sims = []
+
+
+def num_sims(wordd, simm):
+    wordd = wordd.lower()
+    number = wordd.count(simm)
+    res = []
+    sp_word = [a for a in wordd]
+    for i in range(number):
+        indexx = sp_word.index(simm) + i
+        res.append(indexx)
+        del sp_word[indexx - i]
+    return res
+
+
+def vis(form, lis_t, simv):
+    form = [a for a in form]
+    for i in range(len(lis_t)):
+        form[lis_t[i]] = simv
+    return ''.join(form)
+
+
+print('Я загадала слово, можем начинать. Чтобы завершить, напишите "я сдаюсь"')
+frm = "".ljust(len(word), "_")
+while finish:
+    sim = input()
+    if sim.lower() == 'я сдаюсь':
+        print(f"Жаль...:( Я загадывала слово '{word}'")
+        finish = False
+    elif sim.lower() == word.lower():
+        print("Поздравляю!! Ты угадал слово. Получи подарок - 🎁")
+        finish = False
+
+    elif sim in used_sims:
+        print("Вы уже использовали этот символ")
+    elif sim in sims:
+        if sim.lower() in word.lower():
+            spis = num_sims(word, sim.lower())
+            frm = vis(frm, spis, sim)
+            used_sims.append(sim.lower())
+            if "_" not in frm:
+                print(f"Поздравляю!! Я загадывала слово '{word}' Получи подарок - 🎁")
+                finish = False
+            else:
+                print(frm)
+        else:
+            used_sims.append(sim.lower())
+            print("Этой буквы нет в слове :( Попробуйте еще раз ")
+
+    else:
+        print('ОШИБКА!! Введите ОДНУ букву на кирилице')
