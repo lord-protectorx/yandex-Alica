4bfe01cb58d703225cb5d5dd2d8255ddb0967004
Togrul <togrul.ibragimov.2015@mail.ru>
2022-04-23 18:38:45 +0300

    добавлена первичный многопользовательский доступ (не вылетает ошибка при работе с разными сесиями)

--- main_build/main.py
+++ main_build/main.py
@@ -6,20 +6,22 @@ import random
 
 app = Flask(__name__)
 
-# данные для игры
-spis_word = ["Горилла", "Человек", "Время", "Жизнь", "Вопрос", "Ребенок", "Конец", "Система", "Начало"]
-sims = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
-        'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
-word = random.choice(spis_word)
-finish = True
-used_sims = []
-kolvo = 0
-frm = "".ljust(len(word), "_")
-print(word)
+
+class GameData():
+    def __init__(self):
+        # данные для игры
+        self.spis_word = ["Горилла", "Человек", "Время", "Жизнь", "Вопрос", "Ребенок", "Конец", "Система", "Начало"]
+        self.sims = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
+                     'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+        self.word = random.choice(self.spis_word)
+        self.finish = True
+        self.used_sims = []
+        self.kolvo = 0
+        self.frm = "".ljust(len(self.word), "_")
+
 
 @app.route('/post', methods=['POST'])
 def handler():
-
     with open('../data/last request.json', 'w') as file:
         json.dump(request.json, file)
 
@@ -33,12 +35,14 @@ def handler():
     handle_dialog(response, request.json)
     return response
 
+
 def handle_dialog(resp, reqst):
+    global default_game_data
     # проверка, что это не первое сообщение
     if reqst['request']['original_utterance']:
-        game_data = game()
-        resp['response']['text'] = game_data[0]
-        if game_data[1] is False:
+        game_result = game()
+        resp['response']['text'] = game_result[0]
+        if game_result[1] is False:
             resp['response']['end_session'] = True
 
     else:
@@ -46,10 +50,13 @@ def handle_dialog(resp, reqst):
         resp['response']['text'] = 'Привет! Давай сыграем в виселицу с городами.' \
                                    'Я загадал слово, можем начинать. Чтобы завершить, напишите "я сдаюсь"'
 
+        # создаю объект класса с дефолтными данными в нем
+        default_game_data = GameData()
+        print(default_game_data.word)
+
 
 def game():
-    # долбаные глобалы
-    global finish, kolvo, frm
+    global default_game_data
 
     def num_sims(wordd, simm):
         wordd = wordd.lower()
@@ -68,38 +75,38 @@ def game():
             form[lis_t[i]] = simv
         return ''.join(form)
 
-    while finish:
+    while default_game_data.finish:
         sim = request.json["request"]["command"]
         if sim.lower() == 'я сдаюсь':
-            finish = False
-            return (f"Жаль...:( Я загадывала слово '{word}'", False)
+            default_game_data.finish = False
+            return (f"Жаль...:( Я загадывала слово '{default_game_data.word}'", False)
 
-        elif sim.lower() == word.lower():
-            finish = False
+        elif sim.lower() == default_game_data.word.lower():
+            default_game_data.finish = False
             return ("Поздравляю!! Ты угадал слово. МОЛОДЕЦ", False)
 
-        elif sim in used_sims:
+        elif sim in default_game_data.used_sims:
             return ("Вы уже использовали этот символ", True)
-        elif sim.lower() in sims:
-            if sim.lower() in word.lower():
-                kolvo = 0
-                spis = num_sims(word, sim.lower())
-                frm = vis(frm, spis, sim)
-                used_sims.append(sim.lower())
-                if "_" not in frm:
+        elif sim.lower() in default_game_data.sims:
+            if sim.lower() in default_game_data.word.lower():
+                default_game_data.kolvo = 0
+                spis = num_sims(default_game_data.word, sim.lower())
+                default_game_data.frm = vis(default_game_data.frm, spis, sim)
+                default_game_data.used_sims.append(sim.lower())
+                if "_" not in default_game_data.frm:
                     finish = False
-                    return (f"Поздравляю!! Я загадывала слово '{word}'", False)
+                    return (f"Поздравляю!! Я загадывала слово '{default_game_data.word}'", False)
 
                 else:
-                    return (frm, True)
+                    return (default_game_data.frm, True)
             else:
-                if kolvo == 3:
+                if default_game_data.kolvo == 3:
                     finish = False
-                    return (f"Вы пробовали слишком много раз :) Я загадывала слово '{word}'.", False)
+                    return (f"Вы пробовали слишком много раз :) Я загадывала слово '{default_game_data.word}'.", False)
 
                 else:
-                    used_sims.append(sim.lower())
-                    kolvo += 1
+                    default_game_data.used_sims.append(sim.lower())
+                    default_game_data.kolvo += 1
                     return ("Этой буквы нет в слове :( Попробуйте еще раз ", True)
 
         else:
